fd4e08bf4e4cb5b4a01bfb4177f4fa3f31ad5308
Patrick Balsiger <frupii@gmail.com>
2017-01-28 17:39:05 +0000

    status change event

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const slimple = require('slimple');
 
 slimple.run({
     port: 8080,
-    contentPath: '/content',
     servicePath: '/services'
 });
--- services/mplayer/MplayerApi.js
+++ services/mplayer/MplayerApi.js
@@ -1,19 +1,28 @@
-var spawn = require('child_process').spawn;
+var WebSocketClient = require('websocket').client;
+var MPlayer = require('mplayer');
 
 module.exports = MplayerApi = {
     resource: '/api',
     POST: (ctx, http) => {
+	console.log(http.data);
         let mode = http.data.mode; // todo handle modes
-        ctx.mPlayerInstance = spawn('mplayer', [
-            '-playlist', http.data.playlist
-        ]);
-        /*
-        child.stdout.on('data', function(chunk) {
-          // output will be here in chunks
-        });
-        */
+        if(!ctx.mPlayerInstance){
+		ctx.mPlayerInstance = new MPlayer();
+	}
+	ctx.mPlayerInstance.on('status', (data) => {
+		var client = new WebSocketClient();
+		client.on('connect', (connection) => {
+			console.log('wsc send ' + data);
+			connection.send(data);
+			connection.disconnect();
+		});
+		client.connect('ws://conductor:1880/wss');
+	});
+	ctx.mPlayerInstance.openPlaylist('http://ipx.psyradio.org:8010/listen.pls');
+	http.reply({});
     },
     DELETE: (ctx, http) => {
-        ctx.mPlayerInstance.kill('SIGHUP');
+        ctx.mPlayerInstance.stop();
+	http.reply({});
     }
 };
