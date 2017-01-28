d0d8e1f0ef52673177dca5ced535bde58dd94195
Patrick Balsiger <frupii@gmail.com>
2017-01-28 18:44:44 +0100

    fix websocket stuff

--- services/mplayer/MplayerApi.js
+++ services/mplayer/MplayerApi.js
@@ -4,25 +4,35 @@ var MPlayer = require('mplayer');
 module.exports = MplayerApi = {
     resource: '/api',
     POST: (ctx, http) => {
-	console.log(http.data);
+        console.log(http.data);
         let mode = http.data.mode; // todo handle modes
-        if(!ctx.mPlayerInstance){
-		ctx.mPlayerInstance = new MPlayer();
-	}
-	ctx.mPlayerInstance.on('status', (data) => {
-		var client = new WebSocketClient();
-		client.on('connect', (connection) => {
-			console.log('wsc send ' + data);
-			connection.send(data);
-			connection.disconnect();
-		});
-		client.connect('ws://conductor:1880/wss');
-	});
-	ctx.mPlayerInstance.openPlaylist('http://ipx.psyradio.org:8010/listen.pls');
-	http.reply({});
+        if (!ctx.mPlayerInstance) {
+            ctx.mPlayerInstance = new MPlayer();
+            ctx.ws = {
+                client: new WebSocketClient()
+            };
+            ctx.wsc.on('connect', (connection) => {
+
+                ctx.ws.connection = connection;
+            });
+            ctx.ws.client.connect('ws://conductor:1880/wss');
+        }
+        ctx.mPlayerInstance.on('status', (data) => {
+            let title = data.title;
+            if (title) {
+                console.log('wsc send ' + title);
+                ctx.ws.connection.send({
+                    topic: 'rpi.music',
+                    status: data,
+                    title: title
+                });
+            }
+        });
+        ctx.mPlayerInstance.openPlaylist('http://ipx.psyradio.org:8010/listen.pls');
+        http.reply({});
     },
     DELETE: (ctx, http) => {
         ctx.mPlayerInstance.stop();
-	http.reply({});
+        http.reply({});
     }
 };
