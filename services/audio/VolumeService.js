ca1af19a28d10a8f4fc11d4b2f99a58794d1e570
Patrick Balsiger <frupii@gmail.com>
2017-01-30 12:28:43 +0100

    volume service

--- services/audio/VolumeService.js
+++ services/audio/VolumeService.js
@@ -3,6 +3,7 @@ const exec = require('child_process').exec;
 module.exports = VolumeService = {
     resource: '/volume',
     POST: (ctx, http) => {
+        console.log(http.data);
         exec('amixer set PCM -- ' + http.data.volumeLevel + '%');
         http.reply({});
     }
